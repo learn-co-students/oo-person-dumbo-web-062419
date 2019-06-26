e3903a8279e07d7242288020ff6fadfa1c8e978b
github cjl248 <github cjl248@cornell.edu>
2019-06-26 15:12:17 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # your code goes here
+class Person
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+
+    def happiness=(happiness)
+        if happiness > 10 
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else 
+            @happiness = happiness
+        end
+    end
+
+end
