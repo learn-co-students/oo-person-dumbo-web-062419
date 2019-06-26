d7e00f8ed504aa80ac6a2287cbeedf5f27744a04
John Fajardo <fajardocj@gmail.com>
2019-06-26 15:12:35 -0400

    Hitting our btts

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name, balance=25, happiness=8)
+        @name = name
+        @bank_account = balance
+        @happiness = happiness.clamp(1,10)
+    end
+
+    # def clamp(happiness, 1, 10)
+    #     [happiness, 1,10].sort[1]
+    # end
+
+
+end
